e34673b986b16ed6685daaccdbd8b82ea93f3264
SN-Development <126416494+SN-Development@users.noreply.github.com>
2023-07-04 17:47:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      <BrowserRouter>
-          <App></App>
+           <HomePage></HomePage>
        </BrowserRouter>
   </React.StrictMode>
 );
